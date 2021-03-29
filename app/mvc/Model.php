da146914bbdd54863244c0150644f981d00b255b
Elena Ferrer <elenaferrercampos@gmail.com>
2021-03-29 14:55:05 +0200

    commit

--- app/mvc/Model.php
+++ app/mvc/Model.php
@@ -23,10 +23,10 @@ class Model extends PDO
         $result->fetch();
         return $result;
     }
-    public function setRegistro($nomUser, $apUser, $email, $user, $pwd, $bio = 'NULL', $pfp = 'NULL')
+    public function setRegistro($nomUser, $apUser, $email, $user, $pwd, $bio = NULL, $pfp = NULL)
     {
-        $consultaUsu = "insert into users (nomUser, apUser, email, user, pwd, bio, pfp) values ($nomUser, 
-        $apUser, $email, $user, $pwd, $bio, $pfp)";
+        $consultaUsu = "insert into users (nomUser, apUser, email, user, pwd, bio, pfp) values (:nomUser, 
+        :apUser, :email, :user, :pwd, :bio, :pfp)";
         $result = $this->conexion->prepare($consultaUsu);
         $result->bindParam(':nomUser', $nomUser);
         $result->bindParam(':apUser', $apUser);
@@ -40,7 +40,34 @@ class Model extends PDO
         return $result->fetch();
     }
 
-    public function setAlergenos()
+    public function setAlergenos($gluten = '0', $crustaceos = '0', $huevos = '0', $pescado = '0', $cacahuetes = '0',
+    $soja = '0', $lactosa = '0', $frutosdecascara = '0', $apio = '0', $mostaza = '0', $sesamo = '0', $sulfitos = '0',
+    $moluscos = '0', $altramuces = '0', $vegan = '0', $vegetarian='0', $idUser = null, $idReceta = null)
     {
+        $consulta = "insert into alergenos (gluten, crustaceos, huevos, pescado, cacahuetes, soja, lactosa,
+        frutosdecascara, apio, mostaza, sesamo, sulfitos, moluscos, altramuces, vegan, vegetarian, idUser,
+        idReceta) values (:gluten, :crustaceos, :huevos, :pescado, :cacahuetes, :soja, :lactosa, :frutosdecascara, 
+        :apio, :mostaza, :sesamo, :sulfitoa, :moluscos, :altramuces, :vegan, :vegetarian, :idUser, :idReceta)";
+        $result = $this->conexion->prepare($consulta);
+        $result->bindParam(':gluten', $gluten);
+        $result->bindParam(':crustaceos', $crustaceos);
+        $result->bindParam(':huevos', $huevos);
+        $result->bindParam(':pescado', $pescado);
+        $result->bindParam(':cacahuetes', $cacahuetes);
+        $result->bindParam(':soja', $soja);
+        $result->bindParam(':lactosa', $lactosa);
+        $result->bindParam(':frutosdecascara', $frutosdecascara);
+        $result->bindParam(':apio', $apio);
+        $result->bindParam(':mostaza', $mostaza);
+        $result->bindParam(':sesamo', $sesamo);
+        $result->bindParam(':sulfitos', $sulfitos);
+        $result->bindParam(':moluscos', $moluscos);
+        $result->bindParam(':altramuces', $altramuces);
+        $result->bindParam(':vegan', $vegan);
+        $result->bindParam(':vegetarian', $vegetarian);
+        $result->bindParam(':idUser', $idUser);
+        $result->bindParam(':idReceta', $idReceta);
+        $result->execute();
+        return $result->fetch();
     }
 }
